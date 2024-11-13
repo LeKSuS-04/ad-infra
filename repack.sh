5c874c5766a4fbb1b749812d578bd8074102450d
Alexey Tarasov <tarasovion2004@gmail.com>
2024-11-13 14:32:36 +0000

    modify repack.sh

--- repack.sh
+++ repack.sh
@@ -16,9 +16,9 @@ for generated_path in $zips; do
 
     tmpdir=$(mktemp -d)
     7z x -p"$PASSWORD" -o"$tmpdir" "$generated_path"
-    cd $tmpdir
-    zip -P "$PASSWORD" -r "$filename" $(ls)
+    cd "$tmpdir"
+    zip -P "$PASSWORD" -r "$filename" "$(ls)"
     mv "$filename" "$pwd/$generated_path"
 
-    cd $pwd    
+    cd "$pwd"
 done
